07e400f9f80ec9960d6efcd7de166f702b77da57
Leakune <lfavier@live.fr>
2022-07-08 16:40:58 +0200

    tmp

--- app/src/main/java/com/ludovic/android_4a_moc_2022/MainActivity.kt
+++ app/src/main/java/com/ludovic/android_4a_moc_2022/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.ludovic.android_4a_moc_2022
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private lateinit var navController: NavController
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
+
+    private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
+    private var showOneTapUI = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,13 +45,15 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         // hide bottom nav at authentication page
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if(destination.id == R.id.authenticationFragment) {
-                toolbar.visibility = View.GONE
+                //toolbar.visibility = View.GONE
                 bottomNavigationView.visibility = View.GONE
             } else {
                 toolbar.visibility = View.VISIBLE
                 bottomNavigationView.visibility = View.VISIBLE
             }
         }
+
+        configureFirebaseAuthGoogle();
 
 
     }
@@ -49,6 +63,55 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
+    fun configureFirebaseAuthGoogle(){
+        oneTapClient = Identity.getSignInClient(this)
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(
+                BeginSignInRequest.PasswordRequestOptions.builder()
+                    .setSupported(true)
+                    .build())
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.web_client_id))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            // Automatically sign in when exactly one credential is retrieved.
+            .setAutoSelectEnabled(true)
+            .build()
+
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQ_ONE_TAP -> {
+                try {
+                    // ...
+                } catch (e: ApiException) {
+                    when (e.statusCode) {
+                        CommonStatusCodes.CANCELED -> {
+                            Log.d("TAG", "One-tap dialog was closed.")
+                            // Don't re-prompt the user.
+                            showOneTapUI = false
+                        }
+                        CommonStatusCodes.NETWORK_ERROR -> {
+                            Log.d("TAG", "One-tap encountered a network error.")
+                            // Try again or just ignore.
+                        }
+                        else -> {
+                            Log.d("TAG", "Couldn't get credential from result." +
+                                    " (${e.localizedMessage})")
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 
 }
--- app/src/main/java/com/ludovic/android_4a_moc_2022/fragments/AuthenticationFragment.kt
+++ app/src/main/java/com/ludovic/android_4a_moc_2022/fragments/AuthenticationFragment.kt
@@ -1,22 +1,98 @@
 package com.ludovic.android_4a_moc_2022.fragments
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.ludovic.android_4a_moc_2022.BuildConfig
 import com.ludovic.android_4a_moc_2022.R
 
 class AuthenticationFragment : Fragment(R.layout.authentication_fragment){
+    val auth = Firebase.auth
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val connectionBtn = view.findViewById<Button>(R.id.connection_submit_button);
+        val loginField = view.findViewById<EditText>(R.id.authentication_login_text_field);
+        val passwordField = view.findViewById<EditText>(R.id.authentication_password_text_field);
 
-        connectionBtn.setOnClickListener{
-            val action = AuthenticationFragmentDirections.actionAuthenticationFragmentToItinarySearchFragment();
-            view.findNavController().navigate(action)
+        val submitBtn = view.findViewById<Button>(R.id.authentication_submit_button);
+
+        submitBtn.setOnClickListener{
+            val loginFieldValue: String = loginField.text.toString().trim();
+            val passwordFieldValue = passwordField.text.toString().trim();
+
+            if(loginFieldValue == "" || passwordFieldValue == ""){
+                Toast.makeText(context, R.string.error_credentials_null, Toast.LENGTH_SHORT).show();
+                return@setOnClickListener;
+            }
+            auth.signInWithEmailAndPassword(loginFieldValue, passwordFieldValue).addOnCompleteListener {
+                if(it.isSuccessful){
+                    Log.d("ESGI", auth.currentUser!!.email!!)
+                    val action = AuthenticationFragmentDirections.actionAuthenticationFragmentToItinarySearchFragment();
+                    view.findNavController().navigate(action)
+                }
+            }
+
         }
+
+
+//
+//        auth.createUserWithEmailAndPassword("ludo@live.fr", "password").addOnCompleteListener {
+//            if(it.isSuccessful){
+//                Log.d("ESGI", auth.currentUser!!.email!!)
+//            }
+//        }
+//        if(auth.currentUser != null){
+//            auth.signOut()
+//        }
+
+        // 1) Lancer la popup
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(BuildConfig.GOOGLE_API_KEY)
+            .requestEmail()
+            .build()
+
+        startActivityForResult(GoogleSignIn.getClient(requireActivity(), gso).signInIntent, 100)
+
+
     }
+//         2) Recevoir le résultat (vous pouvez ignorer les deprecated)
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                val account = task.getResult(ApiException::class.java)!!
+                val idToken = account.idToken
+
+                Firebase.auth.signInWithCredential(GoogleAuthProvider.getCredential(idToken, null))
+                    .addOnCompleteListener(requireActivity()) { task ->
+                        if (task.isSuccessful) {
+                            // Connecté
+                        } else {
+                            // Il y a une erreur
+                        }
+                    }
+
+            }
+        }
+
 
 }
